cfd986bee2047b4d4e63f5cb13e9eafc7a8a27e0
Cory Adams <corya0687@gmail.com>
2018-05-17 09:01:04 -0400

    added date picker and event adding manual test logic

--- client/App.js
+++ client/App.js
@@ -2,39 +2,76 @@ import React, { Component } from 'react';
 import {
   TextInput, Platform, StyleSheet, Text, View, Alert, Button, Image
 } from 'react-native';
-import {signIn} from './components/actions/api/googleAuth'
-import * as calendarClient from './components/actions/api/googleCalendar'
+import { connect}  from "react-redux"
+import {fetchUser} from './store/actions/userActions'
+import DatePicker from 'react-native-datepicker'
+import GCalEvent from './store/actions/api/googleCalEvent'
+import GoogleCalendar from './store/actions/api/googleCalendar'
+
+@connect((store) =>{
+  return {
+    user: store.user,
+    calendar: store.calendar
+  };
+})
 
 export default class App extends Component<{}> {
-  constructor (props) {
+  constructor(props) {
     super(props)
-    this.state = {
-      "currentUser":{},
-      "calendarName": null,
-      "accessToken": null
-    }
   }
 
   render() {
+    const googleSignIn = () => this.props.dispatch(fetchUser());
+    const isLoggedIn = !!this.props.user.user.id;
+    let postEvent;
+    if (isLoggedIn) {
+      let token = this.props.user.user.accessToken;
+      let calendarId = this.props.user.user.calendarId
+      postEvent = input => {
+        let summary = input.nativeEvent.text
+        let calEvent = new GCalEvent(summary,`${summary} event for today`, new Date().toISOString(), new Date().toISOString(), this.props.user.user.email)
+        GoogleCalendar.addEvent(token, calendarId, calEvent)
+      }
+    }
 
-    const googleSignIn = signIn.bind(this)
-
-    // const googleCalendars = listCalendars.bind(null, this.manager)
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Current User Email
-          {this.state.currentUser.email}
-        </Text>
-        { !this.state.currentUser.email ?
-            <Button onPress={googleSignIn} title="login"/> :
-              <View>
-                <Text>Add Event</Text>
-                <TextInput
-                  style={{height: 40, borderColor: 'gray', borderWidth: 1}}        onChangeText={(calendarName) => this.setState({calendarName})}
-                  value={this.state.calendarName}
-                />
-              </View>
+        { isLoggedIn ?
+            <View>
+              <Text style={styles.welcome}>
+                Current User Email
+                {this.props.user.user.email}
+              </Text>
+              <Text>Add Event</Text>
+              <DatePicker
+                style={{width: 100}}
+                date=''
+                mode="date"
+                placeholder="select date"
+                format="YYYY-MM-DD"
+                minDate="2016-05-01"
+                maxDate="2016-06-01"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                customStyles={{
+                  dateIcon: {
+                    position: 'absolute',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0
+                  },
+                  dateInput: {
+                    marginLeft: 36
+                  }
+                  // ... You can check the source to find the other keys.
+                }}
+                onDateChange={(date) => {this.setState({date: date})}}
+              ></DatePicker>
+              <TextInput
+                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                onSubmitEditing={postEvent}
+              />
+            </View> : <Button onPress={googleSignIn} title="login"/>
         }
       </View>
     );
--- client/index.js
+++ client/index.js
@@ -1,4 +1,16 @@
 import { AppRegistry } from 'react-native';
+import React, { Component } from 'react';
 import App from './App';
+import {Provider} from "react-redux"
+import store from './store/store'
+import {connect} from 'react-redux'
 
-AppRegistry.registerComponent('gritize', () => App);
+GritizeApp = connect()(App)
+
+const Root = () => (
+  <Provider store={store}>
+    <GritizeApp/>
+  </Provider>
+)
+
+AppRegistry.registerComponent('gritize', () => Root );
